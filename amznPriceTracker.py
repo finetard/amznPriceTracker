b8a75b0fd5ca4982e641f804e780fa27af5e3790
Sreedev S <13225616+finetard@users.noreply.github.com>
2020-08-31 19:03:48 +0530

    Updated to fetch category details of the produts
    Updated to fetch the cateogry details of the products.
    
    Writes the output to a file instead of printing it out.
    
    Corrected sys.argv length check

--- amznPriceTracker.py
+++ amznPriceTracker.py
@@ -5,12 +5,12 @@
 Parameters
 ----------
 filename : str
-    The file location of the urls
+    The file name of the file containing urls
 
 Returns
 -------
-list
-    a list of strings representing the Title and Price of Products
+file : priceDetails.txt
+    A file containing Name of the item, category of item, and price
 """
 
 
@@ -22,10 +22,12 @@ def main(filename):
     from bs4 import BeautifulSoup
 
     #use your user-agent . If you don't know search in Google "My user agent"
-    header =  {"User-Agent":'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0'}
+    header =  {"User-Agent":'fill in your user agent'}
 
     file1 = open(filename, 'r') 
     urls = file1.readlines() 
+    file1.close()
+    file2 = open('priceDetails.txt','w')
     #iterating through the urls provided in the file
     for URL in urls:
         #checking whether URL is valid or not
@@ -40,21 +42,24 @@ def main(filename):
         
         soup = BeautifulSoup(page.content, 'html.parser')
 
-        title = soup.find(id="productTitle").get_text()
+        title = soup.find(id="productTitle").get_text().strip()
+        #category = soup.find("span", class_="cat-link")
+        #if category == None:
+        category = soup.find_all('a', class_='a-link-normal a-color-tertiary')
         #Checking whether price detail is given for the product
         try:
             price = soup.find(id="priceblock_ourprice").get_text()
             price = price[1:]
         except:
             price = "Price Not Found"
-
-        print(title.strip())
-        print(price.strip())
-
-
-if __name__ == "__main__":
+        file2.writelines(title + '\n')
+        for cat in category:
+            file2.writelines(cat.get_text().strip() + '\n')
+        file2.writelines(price.strip() + '\n') 
+    file2.close()
+if __name__ == "__main__":  
     #Checking whether the filename is passed as an argument
-    if len(sys.argv[1]) > 1:
+    if len(sys.argv) > 1:
        main(sys.argv[1])
     else:
         print("usage: python scrapper.py filename.txt")
